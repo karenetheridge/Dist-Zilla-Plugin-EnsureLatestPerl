f2a356d1a6f4d0ec3664e22d20d98d93b5496db8
Karen Etheridge <ether@cpan.org>
2018-03-04 14:25:49 -0800

    increment $VERSION after 0.007 release

--- lib/Dist/Zilla/Plugin/EnsureLatestPerl.pm
+++ lib/Dist/Zilla/Plugin/EnsureLatestPerl.pm
@@ -5,7 +5,7 @@ package Dist::Zilla::Plugin::EnsureLatestPerl;
 # ABSTRACT: Ensure the author is releasing using the latest Perl
 # KEYWORDS: plugin release develop author perl version latest
 
-our $VERSION = '0.007';
+our $VERSION = '0.008';
 
 use Moose;
 with 'Dist::Zilla::Role::BeforeRelease';
